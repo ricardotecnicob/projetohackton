f63c92ec6260fda68fc1caa9ca366040398cb96d
Ricardo Alves <ricardotecnicoeletronica@gmail.com>
2020-03-14 13:11:55 -0300

    Desafio das Statups

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,23 +1,39 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import { Container, ContainerConteudo, GroupInfo, FormInputs, ContainerConteudoInterno } from './styles';
 
 export default function Login() {
+
+  const [pageTab,setPageTab] = useState('login');
+
+  const Tabs = value => {
+    setPageTab(value);
+  } 
+
   return (
     <Container >
         <ContainerConteudo>
-          <ContainerConteudoInterno>
+          <ContainerConteudoInterno activoaba={pageTab} >
             <div className="areaConteudo01">
                 <div className="optionsForm">
-                    <button type="buttom" className="opt01" >LOGIN</button>
-                    <button type="buttom" className="opt02" >CADASTRO</button>
+                    <button type="buttom" className="opt01" onClick={() => Tabs('login')} >LOGIN</button>
+                    <button type="buttom" className="opt02" onClick={() => Tabs('cadastro')} >CADASTRO</button>
                 </div>
                 <FormInputs>
+                    {
+                      pageTab === 'cadastro' ? 
+                      <GroupInfo>
+                        <input type="text" placeholder="Nome Statup" />
+                      </GroupInfo>
+                      :
+                      ''
+                    }
+                   
                     <GroupInfo>
-                        <input type="email" placeholder="E-mail do Usuário" />
+                        <input type="email" placeholder="E-mail da Statup" />
                     </GroupInfo>
                     <GroupInfo>
-                        <input type="password" placeholder="Senha do Usuário" />
+                        <input type="password" placeholder="Senha da Statup" />
                     </GroupInfo>
                     <GroupInfo>
                         <input type="buttom" value="Entrar"  />
--- src/pages/Login/styles.js
+++ src/pages/Login/styles.js
@@ -21,7 +21,69 @@ export const ContainerConteudo = styled.div`
 `;
 
 export const ContainerConteudoInterno = styled.div`
-  background: #fff;
+  background: rgba(0,206,209,0.5); 
+  padding: 20px;
+  width: 768px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  color: white;
+
+  .areaConteudo01{
+    flex: 1;
+      .optionsForm{
+        margin-bottom: 10px; 
+
+        .opt01{
+          background:  ${props => props.activoaba === 'login' ? '#1E90FF' : '#00B2EE' };
+          border: 1px solid ${props => props.activoaba === 'login' ? '#fff' : '#00B2EE' };
+          border-radius: 5px;
+          margin-right: 10px;
+          padding: 7px;
+          cursor: pointer;
+          color: white;
+          font-weight: bold;
+        }
+
+        .opt01:hover{
+          background: #1E90FF;
+          border: 1px solid #1E90FF;
+        }
+
+        .opt02{
+          background: ${props => props.activoaba === 'cadastro' ? '#1E90FF' : '#00B2EE' };;
+          border: 1px solid ${props => props.activoaba === 'cadastro' ? '#fff' : '#00B2EE' };;
+          border-radius: 5px;
+          margin-right: 10px;
+          padding: 7px;
+          cursor: pointer;
+          color: white;
+          font-weight: bold;
+        }
+
+        .opt02:hover{
+          background: #1E90FF;
+          border: 1px solid #1E90FF;
+        }
+
+      }
+
+  }
+
+  .areaConteudo02{
+    flex: 1;
+
+    strong{
+      font-size: 2em;
+      text-shadow: 5px 5px 20px  #000;
+    }
+
+    small{
+      font-size: 15px;
+    }
+
+  }
+
 `;
 
 export const FormInputs = styled.div`
@@ -29,5 +91,26 @@ export const FormInputs = styled.div`
 
 
 export const GroupInfo = styled.div`
-    
+    input{
+      padding: 10px;
+      margin-bottom: 5px;
+      border-radius: 5px;
+      border: 1px solid #ccc;
+      width: 80%;
+    }
+
+    input[type=buttom]{
+      text-align: center;
+      background: #00BFFF;
+      border: 1px solid #00BFFF;
+      color: white;
+      font-weight: bold;
+      cursor: pointer;
+    }
+
+    input[type=buttom]:hover{
+      background: #1E90FF;
+      border: 1px solid #1E90FF;
+    }
+
 `;
