f34449a5157d4f01ebcad8d8454908ddaa2d8f99
Vitória Piloto <57605180+VitoriaPiloto@users.noreply.github.com>
2020-03-15 19:01:11 -0300

    Add files via upload

---
+++ src/pages/negociacoes/styles.js
@@ -0,0 +1,19 @@
+import styled from 'styled-components';
+
+export const ConteudoBody=styled.div`
+    @media all and(min-width: 1920px){
+  }
+  background: white;
+  padding: 20px;
+  display: flex;
+  flex-wrap: wrap;
+
+.divide{
+    float:left;
+}
+
+.imgDescription{
+    float:right
+}
+
+`;
